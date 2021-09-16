0971d8ccd60fcde7fd47a7815d424773cf4388cf
gParshav <gparshav@gmail.com>
2021-09-16 11:03:47 +0530

    third  commit

--- src/Coin.js
+++ src/Coin.js
@@ -13,10 +13,8 @@ const Coin = ({name, image, symbol, price, volume, priceChange, marketcap}) => {
                 <div className='coin-data'>
                     <p className='coin-price'>${price}</p>
                     <p className='coin-volume'>${volume.toLocaleString()}</p>
-                    {priceChange < 0 ? (<p className='coin-percent red' >{priceChange.
-                    toFixed(2)}%</p> 
-                    ) : (<p className='coin-percent green'>{priceChange.
-                    toFixed(2)}%</p>)}
+                    {priceChange < 0 ? (<p className='coin-percent red' >{priceChange.toFixed(2)}%</p> 
+                    ) : (<p className='coin-percent green'>{priceChange.toFixed(2)}%</p>)}
                     <p className='coin-marketcap'>Mkt Cap: ${marketcap.toLocaleString()}</p>
                 </div>
             </div>
